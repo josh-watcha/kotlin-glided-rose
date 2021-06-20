a167d9886ffbebc3ba7848b7d14fd93d04d34a4e
aiden-watcha <aiden.jin@watcha.com>
2021-06-21 00:10:08 +0900

    test(BackstagePassesItemTest.kt) : name이 Backstage passes인 경우의 테스트 코드 추가

--- src/test/kotlin/com/gildedrose/BackstagePassesItemTest.kt
+++ src/test/kotlin/com/gildedrose/BackstagePassesItemTest.kt
@@ -3,12 +3,12 @@ package com.gildedrose
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class AgedBrieItemTest {
+internal class BackstagePassesItemTest {
 
     @Test
-    fun `sellIn이 0보다 크면 quality가 1 증가한다`() {
+    fun `sellIn이 0보다 크고 6보다 작으면 quality가 3 증가한다`() {
         // given
-        val item = Item("Aged Brie", 1, 6)
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 1, 6)
         val items = arrayOf(item)
         val app = GildedRose(items)
 
@@ -16,27 +16,13 @@ internal class AgedBrieItemTest {
         app.updateQuality()
 
         // then
-        assertEquals(7, item.quality)
+        assertEquals(9, item.quality)
     }
 
     @Test
-    fun `sellIn이 1보다 크면 quality가 1 증가한다`() {
+    fun `sellIn이 6보다 크고 11다 작으면 quality가 2 증가한다`() {
         // given
-        val item = Item("Aged Brie", 1, 6)
-        val items = arrayOf(item)
-        val app = GildedRose(items)
-
-        // when
-        app.updateQuality()
-
-        // then
-        assertEquals(7, item.quality)
-    }
-
-    @Test
-    fun `sellIn이 0이면 quality가 2 증가한다`() {
-        // given
-        val item = Item("Aged Brie", 0, 6)
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 8, 6)
         val items = arrayOf(item)
         val app = GildedRose(items)
 
@@ -48,29 +34,45 @@ internal class AgedBrieItemTest {
     }
 
     @Test
-    fun `sellIn이 0보다 작으면면 quality가 2 증가한다`() {
+    fun `sellIn이 11보다 크고 50보다 작으면 quality가 1 증가한다`() {
         // given
-        val item = Item("Aged Brie", -1, 6)
-        val app = GildedRose(arrayOf(item))
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 14, 6)
+        val items = arrayOf(item)
+        val app = GildedRose(items)
 
         // when
         app.updateQuality()
 
         // then
-        assertEquals(8, item.quality)
+        assertEquals(7, item.quality)
     }
 
     @Test
-    fun `quality는 50보다 커지지 않는다`() {
+    fun `sellIn이 0이면 quality가 0이 된다`() {
         // given
-        val item = Item("Aged Brie", -1, 50)
-        val app = GildedRose(arrayOf(item))
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 6)
+        val items = arrayOf(item)
+        val app = GildedRose(items)
 
         // when
         app.updateQuality()
 
         // then
-        assertEquals(50, item.quality)
+        assertEquals(0, item.quality)
+    }
+
+    @Test
+    fun `sellIn이 0보다 작으면 quality가 0이 된다`() {
+        // given
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", -1, 6)
+        val items = arrayOf(item)
+        val app = GildedRose(items)
+
+        // when
+        app.updateQuality()
+
+        // then
+        assertEquals(0, item.quality)
     }
 }
 
