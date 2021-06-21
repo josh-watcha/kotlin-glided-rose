54167175bb492b2f7932e9b900c81c721269b0f9
aiden-watcha <aiden.jin@watcha.com>
2021-06-21 09:58:38 +0900

    refactor(...) : name 비교 추상화 레벨 동일하게 수정

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,30 +4,31 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         items.forEach { item ->
-            if (item.name == NAME_AGED_BRIE || item.name == NAME_BACKSTAGE_PASSES) {
+            if (item.name == NAME_AGED_BRIE) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            } else if (item.name == NAME_BACKSTAGE_PASSES) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
-                    if (item.name == NAME_BACKSTAGE_PASSES) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
                     }
                 }
+            } else if (item.name == NAME_SULFURAS) {
+
             } else {
                 if (item.quality > 0) {
-                    if (item.name == NAME_SULFURAS) {
-                    } else {
-                        item.quality = item.quality - 1
-                    }
+                    item.quality = item.quality - 1
                 }
             }
 
@@ -36,21 +37,22 @@ class GildedRose(var items: Array<Item>) {
                 item.sellIn = item.sellIn - 1
             }
 
-            if (item.sellIn < 0) {
-                if (item.name == NAME_AGED_BRIE) {
+            if (item.name == NAME_AGED_BRIE) {
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
                     }
-                } else {
-                    if (item.name == NAME_BACKSTAGE_PASSES) {
-                        item.quality = item.quality - item.quality
-                    } else {
-                        if (item.quality > 0) {
-                            if (item.name == NAME_SULFURAS) {
-                            } else {
-                                item.quality = item.quality - 1
-                            }
-                        }
+                }
+            } else if (item.name == NAME_BACKSTAGE_PASSES) {
+                if (item.sellIn < 0) {
+
+                    item.quality = item.quality - item.quality
+                }
+            } else if (item.name == NAME_SULFURAS) {
+            } else {
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1
                     }
                 }
             }
